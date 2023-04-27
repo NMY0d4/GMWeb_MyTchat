00911aed0732e056b2c0f00bc790b52e364e98df
Grégory Marini <greg.marini@hotmail.fr>
2023-04-27 17:42:49 +0200

    initialize a class in setupServer

---
+++ MyTchat-backend/src/setupServer.ts
@@ -0,0 +1,36 @@
+import {
+  Application,
+  json,
+  urlencoded,
+  Response,
+  Request,
+  NextFunction,
+} from 'express';
+import { Server } from 'http';
+
+export class MyTchatServer {
+  constructor(private app: Application) {}
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routeMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {}
+
+  private standardMiddleware(app: Application): void {}
+
+  private routeMiddleware(app: Application): void {}
+
+  private globalErrorHandler(app: Application): void {}
+
+  private startServer(app: Application): void {}
+
+  private createSocketIO(httpServer: Server): void {}
+
+  private startHttpServer(httpServer: Server): void {}
+
+}
