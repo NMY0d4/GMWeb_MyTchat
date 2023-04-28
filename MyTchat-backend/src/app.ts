84320240abda72db1381a136027327af339bc7f4
Grégory Marini <greg.marini@hotmail.fr>
2023-04-28 15:21:40 +0200

    add database connection

--- MyTchat-backend/src/app.ts
+++ MyTchat-backend/src/app.ts
@@ -1,9 +1,10 @@
 import express, { Express } from 'express';
-
 import { MyTchatServer } from './setupServer';
+import databaseConnection from './setupDatabase';
 
 class Application {
   public initialize(): void {
+    databaseConnection();
     const app: Express = express();
     const server: MyTchatServer = new MyTchatServer(app);
     server.start();
