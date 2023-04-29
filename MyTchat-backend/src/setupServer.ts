8dee839a0c9d4efba35ab68ddd6a2ad7eeb9cb64
Grégory Marini <greg.marini@hotmail.fr>
2023-04-29 17:40:35 +0200

    setup error handling

--- MyTchat-backend/src/setupServer.ts
+++ MyTchat-backend/src/setupServer.ts
@@ -19,6 +19,7 @@ import { createAdapter } from '@socket.io/redis-adapter';
 import 'express-async-errors';
 import { config } from './config';
 import applicationRoutes from './routes';
+import { CustomError, IErrorResponse } from './shared/helpers/error-handler';
 
 const SERVER_PORT = '5000';
 
@@ -64,7 +65,28 @@ export class MyTchatServer {
     applicationRoutes(app);
   }
 
-  private globalErrorHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {
+    app.all('*', (req: Request, res: Response) => {
+      res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .json({ meesage: `${req.originalUrl} not found` });
+    });
+
+    app.use(
+      (
+        error: IErrorResponse,
+        _req: Request,
+        res: Response,
+        next: NextFunction
+      ) => {
+        console.log(error);
+        if (error instanceof CustomError) {
+          return res.status(error.statusCode).json(error.serializeErrors());
+        }
+        next();
+      }
+    );
+  }
 
   private async startServer(app: Application): Promise<void> {
     try {
