2c3682d177f5f2338829467f3a3ab9145f87ce32
Grégory Marini <greg.marini@hotmail.fr>
2023-05-02 17:51:13 +0200

    work in signup.ts file

--- MyTchat-backend/src/features/auth/controllers/signup.ts
+++ MyTchat-backend/src/features/auth/controllers/signup.ts
@@ -2,9 +2,12 @@ import { ObjectId } from 'mongodb';
 import { Request, Response, request } from 'express';
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
 import { signupSchema } from '@auth/schemes/signup';
-import { IAuthDocument } from '@auth/interfaces/auth.interface';
+import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface';
 import { authService } from '@service/db/auth.service';
 import { BadRequestError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helpers';
+import { UploadApiResponse } from 'cloudinary';
+import { uploads } from '@global/helpers/cloudinary-upload';
 
 export class Signup {
   @joiValidation(signupSchema)
@@ -15,5 +18,34 @@ export class Signup {
     if (checkIfUserExist) {
       throw new BadRequestError('Invalid credentials');
     }
+
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Helpers.generateRandomIntegers(12)}`;
+    const authData: IAuthDocument = Signup.prototype.signupData({
+      _id: authObjectId,
+      uId,
+      username,
+      email,
+      password,
+      avatarColor
+    });
+    const result: UploadApiResponse = await uploads(avatarImage, `${userObjectId}`, true, true ) as UploadApiResponse;
+
+    if (!result?.public_id) {
+      throw new BadRequestError('file uploads: Error occured. TRy again.');
+    }
+
+  }
+
+  private signupData(data: ISignUpData): IAuthDocument {
+    const { username, email } = data;
+    return {
+      ...data,
+      username: Helpers.firstLetterUppercase(username),
+      email: Helpers.lowerCase(email),
+
+      createdAt: new Date()
+    } as IAuthDocument;
   }
 }
