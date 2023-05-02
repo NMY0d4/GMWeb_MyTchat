a3e9a379686e95c9574823ac8daffd74b5fd4068
Grégory Marini <greg.marini@hotmail.fr>
2023-05-02 16:00:55 +0200

    start create Signup user method

---
+++ MyTchat-backend/src/features/auth/controllers/signup.ts
@@ -0,0 +1,19 @@
+import { ObjectId } from 'mongodb';
+import { Request, Response, request } from 'express';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { signupSchema } from '@auth/schemes/signup';
+import { IAuthDocument } from '@auth/interfaces/auth.interface';
+import { authService } from '@service/db/auth.service';
+import { BadRequestError } from '@global/helpers/error-handler';
+
+export class Signup {
+  @joiValidation(signupSchema)
+  public async create(req: Request, res: Response): Promise<void> {
+    const { username, email, password, avatarColor, avatarImage } = req.body;
+    const checkIfUserExist: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
+
+    if (checkIfUserExist) {
+      throw new BadRequestError('Invalid credentials');
+    }
+  }
+}
