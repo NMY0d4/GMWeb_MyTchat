dd81159445b6acf530064f00baf3f0f5ab7ee050
Grégory Marini <greg.marini@hotmail.fr>
2023-05-09 19:18:15 +0200

    add get reaction route and test endpoints

--- MyTchat-backend/src/features/reactions/routes/reactionRoute.ts
+++ MyTchat-backend/src/features/reactions/routes/reactionRoute.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@reaction/controllers/add-reactions';
 import { Remove } from '@reaction/controllers/remove-reaction';
+import { Get_Reactions } from '@reaction/controllers/get-reactions';
 
 class ReactionRoutes {
   private router: Router;
@@ -10,6 +11,14 @@ class ReactionRoutes {
   }
 
   public routes(): Router {
+    this.router.get('/post/reactions/:postId', authMiddleware.checkAuthentication, Get_Reactions.prototype.reactions);
+    this.router.get(
+      '/post/Single/reaction/username/:username/:postId',
+      authMiddleware.checkAuthentication,
+      Get_Reactions.prototype.singleReactionByUsername
+    );
+    this.router.get('/post/reactions/username/:username', authMiddleware.checkAuthentication, Get_Reactions.prototype.reactionsByUsername);
+
     this.router.post('/post/reaction', authMiddleware.checkAuthentication, Add.prototype.reaction);
     this.router.delete(
       '/post/reaction/:postId/:previousReaction/:postReactions',
