d483edd641d14bf70ec10991284f3b2854865ec6
Grégory Marini <greg.marini@hotmail.fr>
2023-05-15 12:15:32 +0200

    add follower job, queue and update follower controller

--- MyTchat-backend/src/shared/services/db/follower.service.ts
+++ MyTchat-backend/src/shared/services/db/follower.service.ts
@@ -1,10 +1,12 @@
+import { IFollowerDocument } from '@follower/interfaces/follower.interface';
 import { FollowerModel } from '@follower/models/follower.schema';
+import { IQueryComplete, IQueryDeleted } from '@post/interfaces/post.interface';
 import { UserModel } from '@user/models/user.schema';
 import { ObjectId, BulkWriteResult } from 'mongodb';
-import mongoose from 'mongoose';
+import mongoose, { Query } from 'mongoose';
 
 class FollowerService {
-  public async addFollowerToDB(userId: string, followeeId: string, followerdocumentId: ObjectId): Promise<void> {
+  public async addFollowerToDB(userId: string, followeeId: string, username: string, followerdocumentId: ObjectId): Promise<void> {
     const followeeObjectId: ObjectId = new mongoose.Types.ObjectId(followeeId);
     const followerObjectId: ObjectId = new mongoose.Types.ObjectId(userId);
 
@@ -30,6 +32,33 @@ class FollowerService {
     ]);
 
     await Promise.all([users, UserModel.findOne({_id: followeeId})]);
+  }
+
+  public async removeFollowerFromDB(followeeId: string, followerId: string): Promise<void> {
+    const followeeObjectId: ObjectId = new mongoose.Types.ObjectId(followeeId);
+    const followerObjectId: ObjectId = new mongoose.Types.ObjectId(followerId);
+
+    const unfollow: Query<IQueryDeleted & IQueryComplete, IFollowerDocument> = FollowerModel.deleteOne({
+      followeeId: followeeObjectId,
+      followerId: followerObjectId
+    });
+
+    const users: Promise<BulkWriteResult> = UserModel.bulkWrite([
+      {
+        updateOne: {
+          filter: { _id: followerId },
+          update: { $inc: { followingCount: -1 } }
+        }
+      },
+      {
+        updateOne: {
+          filter: { _id: followeeId },
+          update: { $inc: { followersCount: -1 } }
+        }
+      }
+    ]);
+
+    await Promise.all([unfollow, users]);
   }
 }
 
